d62aa898916cc05276d6a20fa0431efb74a84c84
Brenda Alvis <brendaalvis@brendas-mbp-2.lan>
2023-03-07 15:39:32 -0600

    setting up

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+// Style Sheet
+import './styles/App.css';
+
+function App() {
+  return (
+    <div>
+      <header>
+      <nav>
+          <ul>
+            <li><a>About</a></li>
+            <li><a>Services</a></li>
+            <li><a>Gallery</a></li>
+            <li><a>Contact</a></li>
+          </ul>
+        </nav>
+      </header>
+    </div>
+  );
+}
+
+export default App;
